79da6e79e0cb3b669e17d49880a4d2dc317512d1
anaswaranv-cbm <anaswara.nv@clearbridgemobile.com>
2020-08-17 16:33:45 -0400

    initial set up for dynamic ingredient handling

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,19 +3,28 @@ import BurgerIngredient from "./BurgerIngredient/BurgerIngredient";
 import classes from './Burger.module.css'
 
 const burger = (props) => {
-    const transformedIngredients = Object.keys(props.ingredients)
+    let transformedIngredients = Object.keys(props.ingredients)
         .map(igKey => {
             return [...Array(props.ingredients[igKey])].map((_, i) => {
                 return <BurgerIngredient key={igKey + i} type={igKey} />;
-            });
-        }); //create an array based on ingredient key
+            });    //create an array based on ingredient key
+        })
+        .reduce((arr, el) => {
+            return arr.concat(el)} , []); //helps to transform an array to something else
 
+    if(transformedIngredients.length === 0) {
+        transformedIngredients = <div>
+            Please start adding ingredients
+        </div>
+    }
+
+    console.log(transformedIngredients);
     return (
         <React.Fragment>
             <div className={classes.Burger}>
-                <BurgerIngredient type="bread-top"></BurgerIngredient>
+                <BurgerIngredient type="bread-top"/>
                 {transformedIngredients}
-                <BurgerIngredient type="bread-bottom"></BurgerIngredient>
+                <BurgerIngredient type="bread-bottom"/>
             </div>
         </React.Fragment>
     );
