dc8fdf5fbe0f1f197164d31bf22538f82e773ee6
anaswaranv-cbm <anaswara.nv@clearbridgemobile.com>
2020-08-17 16:08:14 -0400

    method for dynamic ingredient

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -2,17 +2,23 @@ import React from 'react';
 import BurgerIngredient from "./BurgerIngredient/BurgerIngredient";
 import classes from './Burger.module.css'
 
-const Burger = (props) => {
+const burger = (props) => {
+    const transformedIngredients = Object.keys(props.ingredients)
+        .map(igKey => {
+            return [...Array(props.ingredients[igKey])].map((_, i) => {
+                return <BurgerIngredient key={igKey + i} type={igKey} />;
+            });
+        }); //create an array based on ingredient key
+
     return (
         <React.Fragment>
             <div className={classes.Burger}>
                 <BurgerIngredient type="bread-top"></BurgerIngredient>
-                <BurgerIngredient type="cheese"></BurgerIngredient>
-                <BurgerIngredient type="salad"></BurgerIngredient>
+                {transformedIngredients}
                 <BurgerIngredient type="bread-bottom"></BurgerIngredient>
             </div>
         </React.Fragment>
     );
 }
 
-export default Burger;
+export default burger;
